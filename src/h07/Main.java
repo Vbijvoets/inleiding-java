3841fdfb34d5e3bd1f68e61a4b4107c25b6f7cf6
vbijvoets <v.bijvoets@gmail.com>
2020-09-09 12:00:46 +0200

    h07 klaar, start h08

---
+++ src/h07/Main.java
@@ -0,0 +1,15 @@
+package h07;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Main extends Applet {
+    public void init(){
+        Dimension dim = new Dimension(400, 300);
+        this.setSize(dim);
+
+    }
+    public void paint(Graphics g) {
+
+    }
+}
---
+++ src/h08/Main.java
@@ -0,0 +1,15 @@
+package h08;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Main extends Applet{
+    public void init(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+
+}
