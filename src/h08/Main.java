68f08013fe696cb6377c1fd2c00a74c03fa0c32d
vbijvoets <v.bijvoets@gmail.com>
2020-09-12 14:32:23 +0200

    h08.1

--- src/h08/Main.java
+++ src/h08/Main.java
@@ -1,30 +1,54 @@
 package h08;
 
+import javax.swing.*;
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
 
 public class Main extends Applet{
-    Button button;
-    String screentext;
+    TextField textField = new TextField("Hier staat wat tekst");
+    Button btnOK = new Button("OK");
+    String textfield;
+    Button btnReset = new Button ("Reset");
 
     public void init(){
-        screentext = "Doet deze knop wel iets?";
-        button = new Button("Klik op mij");
-        ButtonListener bl = new ButtonListener();
-        button.addActionListener(bl);
-        add(button);
-
+        btnOK.addActionListener (new ButtonListener());
+        textField.addActionListener(new TextFieldListener());
+        btnReset.addActionListener(new ButtonListener());
+        add(btnOK);
+        add(btnReset);
+        add(textField);
+        textfield = "";
     }
 
     public void paint(Graphics g){
-        g.drawString(screentext,50,50);
+        g.drawString(textfield,50,50);
     }
 
     class ButtonListener implements ActionListener{
         public void actionPerformed (ActionEvent e){
-            screentext = "Ja hij doet het";
-            repaint();
+            if(e.getSource() == btnOK){
+                textfield = textField.getText();
+                textField.setText("");
+                textField.requestFocus();
+                repaint();
+            }
+            if (e.getSource() == btnReset){
+                textfield = "";
+                textField.setText("");
+                textField.requestFocus();
+                repaint();
+            }
+        }
+    }
+    class TextFieldListener implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            if(e.getSource() == textField){
+                textfield = textField.getText();
+                textField.setText("");
+                textField.requestFocus();
+                repaint();
+            }
         }
     }
 
